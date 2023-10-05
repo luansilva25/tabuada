c6938875a2685ba1824599d9911b90e236961fc4
luansilva25 <luansilva250807@gmail.com>
2023-10-05 11:04:16 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+function exibir(){
+    const div_exibir = document.getElementById('exibir')
+    const exibir = document.getElementById('txt').value
+    div_exibir.innerHTML = `<p>voce digitou: ${exibir}</p>`
+    
+}
+
+function tabuada(){
+    const num= document.querySelector('#num').value
+    const resul=document.querySelector('#resul')
+    let multi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    resul.innerHTML = '';
+    for(const item of multi){
+        const multiplicador = num * item
+        resul.innerHTML +=` a tabuada do ${num} x ${item} = ${multiplicador}<br>`
+     
+    }
+        
+    }
+
+  
